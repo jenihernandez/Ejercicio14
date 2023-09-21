4f48188d5b1132e6d3dd9dc603a69907663f4018
JENIFER HERNANDEZ ORREGO <jeniferhernandez275138@correo.itm.edu.co>
2023-09-20 23:48:29 -0500

    14

---
+++ ejercicio14/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        int sumPares = 0;
+        int sumImpares = 0;
+
+        for (int i = 2; i <= 100; i += 2)
+        {
+            Console.WriteLine(i);
+            sumPares += i;
+        }
+
+        for (int i = 1; i <= 100; i += 2)
+        {
+            Console.WriteLine(i);
+            sumImpares += i;
+        }
+
+        Console.WriteLine("Suma de los números pares: " + sumPares);
+        Console.WriteLine("Suma de los números impares: " + sumImpares);
+    }
+}
